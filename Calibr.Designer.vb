cf1e0b0122b1ce99d736bff1718bcc7ee323b506
Михаил Семенюта <sm-art2015@mail.ru>
2021-11-09 08:49:39 +0300

    5

---
+++ Calibr.Designer.vb
@@ -0,0 +1,30 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Calibr
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        components = New System.ComponentModel.Container
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Text = "Calibr"
+    End Sub
+End Class
---
+++ Macro.vb
@@ -0,0 +1,7 @@
+﻿Module Macro
+    Public CutProgram As List(Of String)
+    Private SawMotorOn = "M3"
+    Public Sub Cut(CutAngle As Double, CutLength As Double)
+        CutProgram.Add(SawMotorOn)
+    End Sub
+End Module
