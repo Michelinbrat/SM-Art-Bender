8c0dddccfa2fdefeaf0166a0bb69c2818f5606c2
СМ-арт <-@SMART-PC>
2021-11-09 18:24:21 +0300

    Macro added

--- Macro.vb
+++ Macro.vb
@@ -1,7 +1,12 @@
 ﻿Module Macro
+
     Public CutProgram As List(Of String)
     Private SawMotorOn = "M3"
     Public Sub Cut(CutAngle As Double, CutLength As Double)
-        CutProgram.Add(SawMotorOn)
+        With CutProgram
+            .Add(SawMotorOn)
+            .Add("M1 S1") ' Пауза 1 с
+        End With
+
     End Sub
 End Module
