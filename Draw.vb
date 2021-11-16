488361a148d38f7109dde5b3633d2a3e35b6ed21
СМ-арт <-@SMART-PC>
2021-11-16 18:19:36 +0300

    Parser added

--- Draw.vb
+++ Draw.vb
@@ -1,27 +1,71 @@
 ﻿Module Draw
     Class SplineSeg
-        Public Property SplineType As String
-        Public Coord1, Coord2, Coord3, Coord4 As System.Drawing.Point
+        Private Property SplineType As String
+        Public Coord1, Coord2, Coord3, Coord4 As Point
         Public Angle As Double
         Public Length As Double
+        Public tempstr As String
 
         Public Sub GetAttrFromString(instr As String)
-            Dim pos1, pos2 As Long
-            If Left(instr, 3) = "$01" Then SplineType = "Curve"
-            firstpos = instr.IndexOf("(")
+            Dim pos1, pos3 As Integer
+            Dim Arrstr() As String
+            If Left(instr, 3) = "$02" Then
+                SplineType = "Curve"
 
-            If Left(instr, 3) = "$02" Then SplineType = "Line"
+                pos1 = instr.IndexOf("(") 'Ищем скобку (
+                pos3 = instr.LastIndexOf(")")
+                tempstr = instr.Substring(pos1 + 1, pos3 - pos1 - 1)
 
-            'Coord1.X = Left()
+                Arrstr = tempstr.Split(";")
+
+                Coord1.X = Arrstr(0)
+                Coord1.Y = Arrstr(1)
+                Coord2.X = Arrstr(2)
+                Coord2.Y = Arrstr(3)
+                Coord3.X = Arrstr(4)
+                Coord3.Y = Arrstr(5)
+                Coord4.X = Arrstr(6)
+                Coord4.Y = Arrstr(7)
+
+                tempstr = instr.Substring(0, instr.IndexOf("("))
+                Arrstr = tempstr.Split(";")
+                Length = Arrstr(1)
+            End If
+
+            If Left(instr, 3) = "$01" Then
+
+                SplineType = "Line"
+                pos1 = instr.IndexOf("(") 'Ищем скобку (
+                pos3 = instr.LastIndexOf(")")
+                tempstr = instr.Substring(pos1 + 1, pos3 - pos1 - 1)
+
+                Arrstr = tempstr.Split(";")
+
+                Coord1.X = Arrstr(0)
+                Coord1.Y = Arrstr(1)
+                Coord2.X = Arrstr(2)
+                Coord2.Y = Arrstr(3)
+
+                tempstr = instr.Substring(0, instr.IndexOf("("))
+                Arrstr = tempstr.Split(";")
+                Length = Arrstr(1)
+                Angle = Arrstr(2)
+            End If
         End Sub
     End Class
     Public Sub DrawLineSegment()
 
     End Sub
     Public Sub Parser()
-        Dim tempstr As String = "$02(558.942,618.93;587.095,607.116;347.298,352.514;347.298,363.024)"
+        Dim tempstr2 As String = "$02;63,303(558,942;618,93;587,095;607,116;347,298;352,514;347,298;363,024)"
+        Dim tempstr As String = "$01;85,226;0(473,715;347,298;558,942;347,298)"
         Dim wa As New SplineSeg
         wa.GetAttrFromString(tempstr)
-        Debug.Print(wa.SplineType)
+        Debug.Print(wa.Coord1.X)
+        Debug.Print(wa.Coord1.Y)
+        Debug.Print(wa.Coord2.X)
+        Debug.Print(wa.Coord2.Y)
+        Debug.Print(wa.Length)
+        Debug.Print(wa.Angle)
     End Sub
 End Module
